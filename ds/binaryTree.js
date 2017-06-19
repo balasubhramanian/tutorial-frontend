361807380acf35c26d96deb282e55db5eb4650c3
Balasubramanian Muthuvelu <balasubhramanian@gmail.com>
2017-06-19 12:01:11 +0530

    formatted and added delete

--- ds/binaryTree.js
+++ ds/binaryTree.js
@@ -1,141 +1,232 @@
-function BinaryTree(){
-	var root;
+function BinaryTree() {
+    var root;
 
-	function Node(data,left,right){
-		this.data = data;
-		this.next = left;
-		this.prev = right;
-	}
+    function Node(data, left, right) {
+        this.data = data;
+        this.next = left;
+        this.prev = right;
+    }
 
-	function addInternal(node,data){
-		if(!node){
-			return new Node(data);
-		}
-		/*
-		if(data<node.data){
-			var newNode = addInternal(node.left,data)
-			if(newNode){
-				node.left = newNode;
-			}
-		}else{
-			var newNode = addInternal(node.right,data)
-			if(newNode){
-				node.right = newNode;
-			}
-		}*/
+    function addInternal(node, data) {
+        if (!node) {
+            return new Node(data);
+        }
+        /*
+        if(data<node.data){
+        	var newNode = addInternal(node.left,data)
+        	if(newNode){
+        		node.left = newNode;
+        	}
+        }else{
+        	var newNode = addInternal(node.right,data)
+        	if(newNode){
+        		node.right = newNode;
+        	}
+        }*/
 
-		if(data<node.data){
-			 node.left = addInternal(node.left,data)
-			
-		}else{
-			 node.right = addInternal(node.right,data)
-		}
-		return node;
-	}
+        if (data < node.data) {
+            node.left = addInternal(node.left, data)
 
-	this.add = function(data){
-		if(!root){
-			root= new Node(data);
-			return;
-		}
-		addInternal(root,data)
-		console.log(root)
-	}
+        } else {
+            node.right = addInternal(node.right, data)
+        }
+        return node;
+    }
 
-
-	this.addWhile = function(data){
-		if(!root){
-			root = new Node(data);
-		}else{
-			var newNode = new Node(data);
-
-			var tmp  = root;
-			while(tmp){
-				if(tmp.data<data){
-					if(tmp.right){
-						tmp = tmp.right
-					}else{
-						tmp.right = newNode;
-						break;
-					}
-				}else{
-					if(tmp.left){
-						tmp = tmp.left
-					}else{
-						tmp.left = newNode;
-						break;
-					}
-				}
-			}
-		}
-
-		console.log(root)
-
-	}
-
-	this.display = function(node){
-		console.log(node.data+' ')
-	}
-
-	this.preOrder = function (node){
-		if(arguments.length == 0) {
-			node = root
-		}
-			if(node == null) return;
-
-			this.display(node)
-			this.preOrder(node.left);
-			this.preOrder(node.right);
-	}
-
-	this.inOrder = function (node){
-		if(arguments.length == 0) {
-			node = root
-		}
-			if(node == null) return;
-
-			this.inOrder(node.left);
-			this.display(node)
-			this.inOrder(node.right);
-	}
-
-	this.postOrder = function (node){
-		if(arguments.length == 0) {
-			node = root
-		}
-		if(node == null) return;
-
-			this.postOrder(node.left);
-			this.postOrder(node.right);
-			this.display(node)
-			
-	}
+    this.add = function(data) {
+        if (!root) {
+            root = new Node(data);
+            return;
+        }
+        addInternal(root, data)
+        console.log(root)
+    }
 
 
+    this.addWhile = function(data) {
+        if (!root) {
+            root = new Node(data);
+        } else {
+            var newNode = new Node(data);
 
-	this.find = function(){
+            var tmp = root;
+            while (tmp) {
+                if (tmp.data < data) {
+                    if (tmp.right) {
+                        tmp = tmp.right
+                    } else {
+                        tmp.right = newNode;
+                        break;
+                    }
+                } else {
+                    if (tmp.left) {
+                        tmp = tmp.left
+                    } else {
+                        tmp.left = newNode;
+                        break;
+                    }
+                }
+            }
+        }
 
-	}
+        console.log(root)
 
-	this.remove = function(){
+    }
 
-	}
+    this.display = function(node) {
+        console.log(node.data + ' ')
+    }
+
+    this.preOrder = function(node) {
+        if (arguments.length == 0) {
+            node = root
+        }
+        if (node == null) return;
+
+        this.display(node)
+        this.preOrder(node.left);
+        this.preOrder(node.right);
+    }
+
+    this.inOrder = function(node) {
+        if (arguments.length == 0) {
+            node = root
+        }
+        if (node == null) return;
+
+        this.inOrder(node.left);
+        this.display(node)
+        this.inOrder(node.right);
+    }
+
+    this.postOrder = function(node) {
+        if (arguments.length == 0) {
+            node = root
+        }
+        if (node == null) return;
+
+        this.postOrder(node.left);
+        this.postOrder(node.right);
+        this.display(node)
+
+    }
+
+
+
+    this.find = function() {
+
+    }
+
+    this.remove = function(data) {
+        root = this.removeInternal(root, data)
+        console.log(root)
+    }
+
+    this.removeInternal = function(node, data) {
+        if (node == null) {
+            return null;
+        }
+        if (node.data == data) {
+            //case 1 : no children
+            if (node.left == null && node.right == null) {
+                return null
+            }
+
+            //case 2 : one child
+            if (node.left == null) {
+                return node.right
+            }
+
+            if (node.right == null) {
+                return node.left
+            }
+            //case 3 : 2 children
+            /**
+             * find the smallest of the right or largest of left
+             * replace the data and ref
+             * delete the smallest
+             */
+
+
+
+
+
+        } else if (data < node.data) {
+            node.left = this.removeInternal(node.left, data)
+        } else {
+            node.right = this.removeInternal(node.right, data)
+        }
+
+        return node;
+
+    }
+
+
+    this.removeInternal1 = function(parent, node, data) {
+        if (node.data == data) {
+
+
+            //case 1 - no childer
+            if (node.left == null && node.right == null) {
+                if (parent.left && parent.left.data == data) {
+                    parent.left = null
+                }
+                if (parent.right && parent.right.data == data) {
+                    parent.right = null;
+                }
+            }
+
+            //case 2 - one child
+            else if (node.left == null || node.right == null) {
+                if (node.left) {
+                    if (parent.left && parent.left.data == data) {
+                        parent.left = node.left
+                    }
+                    if (parent.right && parent.right.data == data) {
+                        parent.right = node.left;
+                    }
+                } else {
+                    if (parent.left && parent.left.data == data) {
+                        parent.left = node.right
+                    }
+                    if (parent.right && parent.right.data == data) {
+                        parent.right = node.right;
+                    }
+                }
+            }
+
+            //case 3
+            else {
+
+            }
+
+        } else {
+
+            if (data < node.data) {
+                this.removeInternal(node, node.left, data)
+            } else {
+                this.removeInternal(node, node.right, data)
+            }
+        }
+
+
+    }
 
 
 }
 
 var tree = new BinaryTree();
 tree.add(5);
-console.log('------------')
-	tree.add(1);
-console.log('------------')
- tree.add(3);
- console.log('------------')
- tree.add(4);
- console.log('------------')
+tree.add(3);
+tree.add(4);
 tree.add(2);
+tree.add(1);
+tree.add(7);
 
 console.log('Printing data ----------')
-//tree.preOrder();
-//tree.inOrder();
-tree.postOrder();
+    //tree.preOrder();
+    //tree.inOrder();
+    //tree.postOrder();
+
+//tree.remove(1)
+tree.remove(2)
